e10e23e70cff462cdaa69f7124bf4b465f54ebfd
Tuğberk Demiralp <92752785+Daelass@users.noreply.github.com>
2023-03-28 00:38:06 +0300

    Add project files.

---
+++ WebAPINorthwind_0/RequestModels/CategoryCreateRequestModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPINorthwind_0.RequestModels
+{
+
+    //Kullanıcı'nın bir Kategori eklemek için nasıl veri göndereceğinin tasarımı
+
+    public class CategoryCreateRequestModel
+    {
+        public string CategoryName { get; set; }
+        public string Description { get; set; }
+    }
+}
